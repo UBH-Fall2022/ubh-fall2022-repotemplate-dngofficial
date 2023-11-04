3c6007e9246dc98213ca2f5b3c6a70e919851eb7
TIRTH <tirthrdesai05@gmail.com>
2023-11-04 14:13:26 -0400

    DATA SCRAPING

---
+++ scrap.py
@@ -0,0 +1,36 @@
+from googleapiclient.discovery import build
+import json
+
+
+def get_comments(youtube, video_id, comments=[], token=''):
+
+    video_response = youtube.commentThreads().list(part='snippet',
+                                                   videoId=video_id,
+                                                   pageToken=token).execute()
+    for item in video_response['items']:
+        comment = item['snippet']['topLevelComment']
+        text = comment['snippet']['textDisplay']
+        comments.append(text)
+    if "nextPageToken" in video_response:
+        return get_comments(youtube, video_id, comments, video_response['nextPageToken'])
+    else:
+        return comments
+
+
+config = open("config.json", "r")
+
+api_key = json.load(config)
+api_key = api_key["APIKEY"]
+video_id = "rwHgE7CEnpE"
+
+youtube = build('youtube', 'v3', developerKey=api_key)
+comment_threads = get_comments(youtube, video_id)
+data = {}
+for i in range(len(comment_threads)):
+    data["comment#" + str(i+1)] = comment_threads[i]
+
+jsonFile = open("comments.json", "w+")
+json_string = json.dumps(data, indent=4)
+jsonFile.write(json_string)
+jsonFile.close()
+print("Completed")
