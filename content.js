02ae67e2a0eccc39fc00346451831d4bcd2a975a
brian-kko <brianko399@gmail.com>
2023-11-04 17:09:42 -0400

    w

--- content.js
+++ content.js
@@ -1,36 +1,52 @@
-// Function to classify comments as hateful
-function classifyComment(commentText) {
-    // Implement your AI model classification logic here
-    return true; // Replace with your classification result (true for hateful, false for non-hateful)
+console.log("Content script is running!");
+const warning = document.createElement("div");
+warning.textContent = "Test Warning Message";
+document.body.appendChild(warning);
+
+// Function to send a message to the content script to reveal a hidden comment
+function revealComment(commentId) {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        chrome.scripting.executeScript({
+            target: { tabId: tabs[0].id },
+            function: (commentId) => {
+                const comment = document.getElementById(commentId);
+                const warningBox = comment.previousElementSibling;
+                comment.style.display = 'block'; // Show the original comment
+                warningBox.style.display = 'none'; // Hide the warning box
+            },
+            args: [commentId],
+        });
+    });
 }
 
-// Iterate through comments and process them
-const comments = document.querySelectorAll('.comment');
-comments.forEach((comment) => {
-    const commentText = comment.textContent;
-    if (classifyComment(commentText)) {
-        // Create a div element for the warning box
-        const warningBox = document.createElement('div');
-        warningBox.className = 'warning-box';
-
-        // Create a warning message
-        const warningMessage = document.createElement('p');
-        warningMessage.textContent = 'This comment may contain hateful content. Click to reveal.';
-
-        // Create a "View" button
-        const viewButton = document.createElement('button');
-        viewButton.textContent = 'View';
-        viewButton.addEventListener('click', () => {
-            comment.style.display = 'block'; // Show the original comment
-            warningBox.style.display = 'none'; // Hide the warning box
-        });
-
-        // Append warning message and "View" button to the warning box
-        warningBox.appendChild(warningMessage);
-        warningBox.appendChild(viewButton);
-
-        // Hide the original comment and append the warning box
-        comment.style.display = 'none';
-        comment.parentNode.insertBefore(warningBox, comment);
+// Add a click event listener to the "View" button
+document.addEventListener('click', function (e) {
+    if (e.target && e.target.tagName === 'BUTTON') {
+        revealComment(e.target.dataset.commentId);
     }
+});
+
+// Iterate through comments and create a warning box for each
+const comments = document.querySelectorAll('.comment');
+comments.forEach((comment, index) => {
+    // Create a div element for the warning box
+    const warningBox = document.createElement('div');
+    warningBox.className = 'warning-box';
+
+    // Create a warning message
+    const warningMessage = document.createElement('p');
+    warningMessage.textContent = 'This comment may contain hateful content. Click to reveal.';
+
+    // Create a "View" button
+    const viewButton = document.createElement('button');
+    viewButton.textContent = 'View';
+    viewButton.dataset.commentId = `comment-${index}`; // Add a unique comment ID to the button
+
+    // Append warning message and "View" button to the warning box
+    warningBox.appendChild(warningMessage);
+    warningBox.appendChild(viewButton);
+
+    // Hide the original comment and append the warning box
+    comment.style.display = 'none';
+    comment.parentNode.insertBefore(warningBox, comment);
 });
