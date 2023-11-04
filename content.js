3c4a777e4092b7e652933e5e14a1f585c7e97bfe
brian-kko <brianko399@gmail.com>
2023-11-04 17:46:15 -0400

    w

--- content.js
+++ content.js
@@ -24,29 +24,5 @@ document.addEventListener('click', function (e) {
     if (e.target && e.target.tagName === 'BUTTON') {
         revealComment(e.target.dataset.commentId);
     }
-});
+});a
 
-// Iterate through comments and create a warning box for each
-const comments = document.querySelectorAll('.comment');
-comments.forEach((comment, index) => {
-    // Create a div element for the warning box
-    const warningBox = document.createElement('div');
-    warningBox.className = 'warning-box';
-
-    // Create a warning message
-    const warningMessage = document.createElement('p');
-    warningMessage.textContent = 'This comment may contain hateful content. Click to reveal.';
-
-    // Create a "View" button
-    const viewButton = document.createElement('button');
-    viewButton.textContent = 'View';
-    viewButton.dataset.commentId = `comment-${index}`; // Add a unique comment ID to the button
-
-    // Append warning message and "View" button to the warning box
-    warningBox.appendChild(warningMessage);
-    warningBox.appendChild(viewButton);
-
-    // Hide the original comment and append the warning box
-    comment.style.display = 'none';
-    comment.parentNode.insertBefore(warningBox, comment);
-});
