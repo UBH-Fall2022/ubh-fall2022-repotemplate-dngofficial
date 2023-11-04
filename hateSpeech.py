880cbc8ed9562822097e5542f040fa0779c6c702
Ethan43443 <125399829+Ethan43443@users.noreply.github.com>
2023-11-04 16:59:50 -0400

    Update hateSpeech.py
    HEEEHEEEEEE

--- hateSpeech.py
+++ hateSpeech.py
@@ -1,7 +1,5 @@
 import requests
 from transformers import pipeline
-classifier = pipeline("sentiment-analysis", model="michellejieli/inappropriate_text_classifier")
-classifier("I see you’ve set aside this special time to humiliate yourself in public.")
 API_TOKEN="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"
 API_URL = "https://api-inference.huggingface.co/models/michellejieli/inappropriate_text_classifier"
 headers = {"Authorization": "Bearer {}".format(API_TOKEN)}
@@ -10,6 +8,15 @@ def query(payload):
 	##The input is a dictionary
 	response = requests.post(API_URL, headers=headers, json=payload)
 	return response.json()
-	
-output = query()
-###Outputs 
+def getScore(in_json):
+	###Note Dict of dict
+	###Parse through and strip just the NSFW
+	score_list=[]
+	for arr in in_json:
+		###[[{},{}]]
+		for dict_in in arr:
+			NSFW_SCORE=dict_in[1]['score']
+			score_list.apend(NSFW_SCORE)
+	return score_list
+output = query("Hi every")###This will be the input for the com
+getScore(output)###<--Use this to harvest the "bad" scores. Can be used to determine bad on Brian's end. 
