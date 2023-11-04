8ac3c78c626130d29fb79508d294987a778e5094
Ethan43443 <125399829+Ethan43443@users.noreply.github.com>
2023-11-04 18:30:25 -0400

    Update hateSpeech.py

--- hateSpeech.py
+++ hateSpeech.py
@@ -1,26 +1,30 @@
+import requests
+import json
 from transformers import pipeline
-API_TOKEN = "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"
+API_TOKEN="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"
 API_URL = "https://api-inference.huggingface.co/models/michellejieli/inappropriate_text_classifier"
 headers = {"Authorization": "Bearer {}".format(API_TOKEN)}
 
-
 def query(payload):
-    # The input is a dictionary
-    response = requests.post(API_URL, headers=headers, json=payload)
-    return response.json()
-
-
+	##The input is a dictionary
+	response = requests.post(API_URL, headers=headers, json=payload)
+	return response.json()
 def getScore(in_json):
-    # Note Dict of dict
-    # Parse through and strip just the NSFW
-    score_list = []
-    for arr in in_json:
-        # [[{},{}]]
-        for dict_in in arr:
-            NSFW_SCORE = dict_in[1]['score']
-            score_list.append(NSFW_SCORE)
-    return score_list
-
-
-output = query("Hi every")
-getScore(output)
+	###Note Dict of dict
+	###Parse through and strip just the NSFW
+	score_dict={}
+	for i in range(len(in_json)):
+		###[[{},{}]]
+		score_dict[in_json[i]]=arr[1].get('score')
+		####Reformats the code
+		####"comment#1":.687
+	return score_dict
+def writeFile(dict_in,fileName):
+	##Takes a JSON in and writes to this JSON file name
+	json_obj=json.dumps(dict_in)
+	with open(fileName,'w') as newFile:
+		newFile.write(json_obj)
+output = query("Hi every")###This will be the input for the com
+new_dict=getScore(output)
+##json_file="BigBirtha.json"
+##writeFile(new_dict,json_file)##
